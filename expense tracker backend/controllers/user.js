d51f483a86b563dcf51222748609918d3edf4154
PaiMax <nishwalbh1997@gmail.com>
2023-08-01 14:23:22 +0530

    password encryption feature added

--- expense tracker backend/controllers/user.js
+++ expense tracker backend/controllers/user.js
@@ -1,27 +1,46 @@
 const user=require('../model/user');
-exports.addUser=(req,res,next)=>{
-    console.log()
-    const name=req.body.name;
-    const email=req.body.email;
-    const password=req.body.password;
+const bcrypt=require('bcrypt');
+exports.addUser=async(req,res,next)=>{
+    try{
+        
+    
+        const name=req.body.name;
+        const email=req.body.email;
+        const password=req.body.password;
+         
 
-    user.create({
-        name:name,
-        email:email,
-        password:password
+        bcrypt.hash(password,10,async(err,hash)=>{
+            console.log(err);
+           try{await  user.create({
+            name:name,
+            email:email,
+            password:hash
+    
+        })
+        res.status(201).json({message:'Successfully create new user'});
+    }
+    catch(err){res.send(err);}
+        })
+    
+    
+    
+    
+    
+       
+        //.then(result=>{
+            //user.findByPk(result.id,{ attributes : ['id','name','email','password']})
+            //.then((user)=>res.send(user))
+            //.catch(err=>{
+                //console.log('hi');console.log(err);});
+        //})
+        
 
-    })
-    .then(result=>{
-        user.findByPk(result.id,{ attributes : ['id','name','email','password']})
-        .then((user)=>res.send(user))
-        .catch(err=>{
-            console.log('hi');console.log(err);});
-    })
-    .catch(err=>{res.send(err);
-    console.log(typeof(err))}
-   )
+    //}
+    
+   
 
 }
+catch(err){console.log(err); res.send(err.data)};}
 
 
 
@@ -30,14 +49,20 @@ exports.checkUser=(req,res,next)=>{
     user.findOne({where:{email:req.body.email}  })
     .then((user)=>{ 
         if(user){
-            if(user.password===req.body.password){
-                res.send('User login Successful');
 
-            }
-            else{
-                res.send("Password doen'nt match");
+            bcrypt.compare(req.body.password,user.password,(err,re)=>{
+                if(!err){
+                    res.send('User login Successful');
+                }
+                else{
+                    res.send("Password doen'nt match");
 
-            }
+                }
+
+            })
+            
+
+            
         }
         else{
             res.send("User does'nt exist");
